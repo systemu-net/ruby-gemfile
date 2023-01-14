3f2dd5f19da987c05e50e63f4c43dda2d8f03503
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2023-01-14 01:23:48 -0500

    Add typescript deps. Fix minor issues

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import { after } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -6,10 +7,12 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  after(() => {
+    vscode.window.showInformationMessage('All tests done!');
+  });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
 });
